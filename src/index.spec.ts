0eb9d9a9a47c38fd52a0f9beec4fa46cea3c729c
Stephen Caldwell <steve@campusexplorer.com>
2022-09-13 10:51:00 -0700

    feat: the guts of request signture building has been extracted to @scaldwell77/aws-signed-fetch

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,6 @@
 import test from 'ava';
-import { Request } from 'cross-fetch';
 
-import { getGraphQLClient, signRequest } from './';
+import { getGraphQLClient } from './';
 
 test('getGraphQLClient()', (t) => {
     const client = getGraphQLClient('https://foo.com', {
@@ -15,77 +14,4 @@ test('getGraphQLClient()', (t) => {
     });
 
     t.truthy(client, 'client returned');
-});
-
-test('signRequest()', async (t) => {
-    const host =
-        'gfaichcrsslcpgcnxgaryagfpw.appsync-api.us-east-1.amazonaws.com'; // spell-checker: disable-line
-    const url = `https://${host}/graphql`;
-    const method = 'POST';
-    const headers = {
-        'Content-Type': 'application/json',
-        'X-My-Test': 'foobar',
-    };
-    const date = new Date(2022, 2, 24, 9, 33, 44);
-
-    const query = /* GraphQL */ `
-        query getUser($userId: String!) {
-            user(userId: $userId) {
-                userId
-                userName
-            }
-        }
-    `;
-    const variables = { userId: '12345' };
-    const body = JSON.stringify({ query, variables });
-    const region = 'us-east-1';
-    const credentials = {
-        /* spell-checker: disable */
-        accessKeyId: 'QEBOPDUODEDEDPCRVHWU',
-        secretAccessKey: 'LDdxaf5TTG350BREOYzkQd26QrqPoZPAzMNKDHjY',
-        sessionToken:
-            'TJYF2ZZQuWdQeCk//////////uByMGIjLpaJ2enPSGPXUD75ULwIO5WeCeYeLGMoiI6vTQNxXwQAldvorrBFeiJmhkHbJ59muzdrWqe1hi5cVQF24bTrwdjEn8vfWfCrdA30f1FMmlff51sptmStqlWXraJEEI1JTtjqqPf2NcLVYSH1fXqMc1QjeOg0YgFtcuVqQiuQ1HwbuYKFFXWaRr7TxsVmiqv5isOuknEq9h3le009jEGB3A4ivMMZyYaIRwhSPH5JGMvg7Ldc9kZCYVAKMkTcI0CG1Fh9wMnRv0mQovaDCckVvXM2aDVFPaQeDYQ3NMIj36mpOOiXEr1HjqCwKCCgPOGnx3ZZbfqtKfkBdzdzhSJS',
-        /* spell-checker: enable */
-    };
-
-    const request = new Request(url, {
-        method,
-        headers,
-        body,
-    });
-
-    const signedRequest = await signRequest(request, {
-        regionProvider: async () => region,
-        credentialProvider: async () => credentials,
-        date,
-    });
-
-    t.is(
-        signedRequest.headers.get('content-type'),
-        'application/json',
-        'Content-Type header set'
-    );
-    t.is(signedRequest.headers.get('host'), host, 'Host header set');
-    t.is(
-        signedRequest.headers.get('x-my-test'),
-        'foobar',
-        'custom header preserved'
-    );
-    t.is(
-        signedRequest.headers.get('x-amz-security-token'),
-        credentials.sessionToken,
-        'x-amz-security-token header added'
-    );
-    t.is(
-        signedRequest.headers.get('x-amz-date'),
-        '20220324T163344Z',
-        'x-amz-date header added'
-    );
-    t.is(
-        signedRequest.headers.get('authorization'),
-        'AWS4-HMAC-SHA256 Credential=QEBOPDUODEDEDPCRVHWU/20220324/us-east-1/appsync/aws4_request, SignedHeaders=content-type;host;x-amz-content-sha256;x-amz-date;x-amz-security-token;x-my-test, Signature=50409102c1bdff80eb31eb26a0944be4bfcb0afbdd8a6378e4d504117d6adbc8', // spell-checker: disable-line
-        'authorization header added'
-    );
-
-    t.pass();
 });
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,5 @@
-import { Sha256 } from '@aws-crypto/sha256-js';
-import {
-    NODE_REGION_CONFIG_FILE_OPTIONS,
-    NODE_REGION_CONFIG_OPTIONS,
-} from '@aws-sdk/config-resolver';
-import { defaultProvider as defaultCredentialProvider } from '@aws-sdk/credential-provider-node';
-import { loadConfig } from '@aws-sdk/node-config-provider';
-import { HttpRequest as AwsHttpRequest } from '@aws-sdk/protocol-http';
-import { SignatureV4 } from '@aws-sdk/signature-v4';
-import type { CredentialProvider, Credentials, Provider } from '@aws-sdk/types';
-import { parseUrl } from '@aws-sdk/url-parser';
-import fetch, { Request } from 'cross-fetch';
+import type { Credentials } from '@aws-sdk/types';
+import { createSignedFetch } from '@scaldwell77/aws-signed-fetch';
 import { GraphQLClient } from 'graphql-request';
 import { omit } from 'lodash';
 
@@ -21,105 +11,18 @@ type Options = Omit<
     'fetch'
 >;
 
-type SignArguments = {
-    regionProvider: Provider<string>;
-    credentialProvider: CredentialProvider;
-    date?: Date;
-};
-
-const getCredentialProvider = (credentials?: Credentials) =>
-    credentials ? async () => credentials : defaultCredentialProvider();
-
-const getRegionProvider = (region?: string) =>
-    region
-        ? async () => region
-        : loadConfig(
-              NODE_REGION_CONFIG_OPTIONS,
-              NODE_REGION_CONFIG_FILE_OPTIONS
-          );
-
-const transformHeaders = (request: Request) => {
-    const newHeaders: AwsHttpRequest['headers'] = {};
-    const excludeHeaders = new Set(['host', 'content-type']);
-    request.headers.forEach((value, key) => {
-        if (excludeHeaders.has(key)) return;
-        newHeaders[key] = value;
-    });
-    newHeaders['content-type'] = 'application/json';
-    newHeaders.host = new URL(request.url).host;
-    return newHeaders;
-};
-
-const buildAwsRequest = (request: Request) =>
-    new AwsHttpRequest({
-        ...parseUrl(request.url),
-        method: 'POST',
-        headers: transformHeaders(request),
-        body: request.body,
-    });
-
-const signAwsRequest = async (
-    request: AwsHttpRequest,
-    signArgs: SignArguments
-) => {
-    const [region, credentials] = await Promise.all([
-        signArgs.regionProvider(),
-        signArgs.credentialProvider(),
-    ]);
-    const signer = new SignatureV4({
-        service: 'appsync',
-        region,
-        sha256: Sha256,
-        credentials,
-    });
-    const signedRequest = await signer.sign(request, {
-        signingDate: signArgs.date,
-    });
-    return signedRequest;
-};
-
-const getSignedHeaders = async (request: Request, signArgs: SignArguments) => {
-    const awsRequest = buildAwsRequest(request);
-    const signedAwsRequest = await signAwsRequest(awsRequest, signArgs);
-    return signedAwsRequest.headers;
-};
-
-export const signRequest = async (
-    request: Request,
-    signArgs: SignArguments
-): Promise<Request> => {
-    const signedHeaders = await getSignedHeaders(request, signArgs);
-    const signedRequest = new Request(request.url, {
-        method: request.method,
-        body: request.body,
-        headers: signedHeaders,
-    });
-    return signedRequest;
-};
-
-const signedFetch =
-    (
-        credentialProvider: CredentialProvider,
-        regionProvider: Provider<string>
-    ) =>
-    async (input: RequestInfo, init?: RequestInit) => {
-        const request = new Request(input, init);
-        const signedRequest = await signRequest(request, {
-            regionProvider,
-            credentialProvider,
-        });
-        return fetch(signedRequest);
-    };
-
 export const getGraphQLClient = (
     url: string,
     options?: Options
 ): GraphQLClient => {
     const clientOptions = omit(options, ['awsCredentials', 'awsRegion']);
-    const credentialProvider = getCredentialProvider(options?.awsCredentials);
-    const regionProvider = getRegionProvider(options?.awsRegion);
+    const fetch = createSignedFetch({
+        service: 'appsync',
+        awsCredentials: options?.awsCredentials,
+        awsRegion: options?.awsRegion,
+    });
     return new GraphQLClient(url, {
         ...clientOptions,
-        fetch: signedFetch(credentialProvider, regionProvider),
+        fetch,
     });
 };
